fa589b8496c82d3e6f0b6ece2f0c2c41cea0333a
Phumin63130500094 <chumphu.phumin@gmail.com>
2022-03-21 23:19:28 +0700

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,18 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+
   rules: {
-    // override/add rules settings here, such as:
-    "vue/require-default-prop": "off",
+    'vue/require-default-prop': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+
+  root: true,
+
+  extends: ['plugin:vue/vue3-recommended', 'prettier', 'plugin:vue/vue3-essential', '@vue/prettier'],
+
+  parserOptions: {
+    ecmaVersion: 2020,
   },
 };
