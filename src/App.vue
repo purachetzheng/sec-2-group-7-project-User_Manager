d3d057fdd8afcc79ea379d2a63c18623262fec35
Purachet Zheng <poorachad2545@gmail.com>
2022-04-15 23:30:45 +0700

    back to white nav bar

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 <template>
     <div class="flex flex-col flex-wrap min-h-screen">
         <nav
-            class="min-w-full bg-white dark:bg-black dark:text-white flex flex-col md:flex-row items-center md:justify-between px-6 py-4 border-b border-b-gray-60 shadow-sm max-w-screen-2xl mx-auto"
+            class="min-w-full bg-white flex flex-col md:flex-row items-center md:justify-between px-6 py-4 border-b border-b-gray-60 shadow-sm max-w-screen-2xl mx-auto"
         >
             <div class="rounded-lg px-3 py-2 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900">
                 <router-link :to="{ name: 'Home' }">Home</router-link>
