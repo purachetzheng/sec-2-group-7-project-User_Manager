2bd947eb732c4a08a777a164c96f20c628bf1b67
JustAnfieldz <anf.patthikarnsakul@gmail.com>
2022-04-16 12:28:44 +0700

    deleteUser function v2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, computed, reactive, nextTick, onBeforeMount, onMounted } from 'vue';
+import RiDeleteBin5Line from '../components/icons/RiDeleteBin5Line.vue'
 //router
 import { useRoute, useRouter } from 'vue-router';
 const { params } = useRoute();
@@ -30,17 +31,22 @@ const registerUser = () => {
 const deleteUser = async () => {
     console.log("delete working!");
     let removeUserId = prompt("Enter user ID that you want to delete.")
-    const res = await fetch(`http://localhost:5000/users/${removeUserId}`, {
-        method: 'DELETE'
-    })
-    if(res.status===200){
-        users.value = users.value.filter((user) => user.id !== removeUserId)
-        console.log('delete succesfully');
-        alert(`Delete succesfully, ID number ${removeUserId} has been removed`)
-        location.reload();
+    if (removeUserId === null){
+        console.log("cancel");
+        return;
     }else{
-        console.log('error, cannot delete');
-        alert(`Delete unsuccessfully, ID number ${removeUserId} is not in the data.`)
+        const res = await fetch(`http://localhost:5000/users/${removeUserId}`, {
+            method: 'DELETE'
+        })
+        if(res.status===200){
+            users.value = users.value.filter((user) => user.id !== removeUserId)
+            console.log('delete succesfully');
+            alert(`Delete succesfully, ID number ${removeUserId} has been removed`)
+            location.reload();
+        }else{
+            console.log('error, cannot delete');
+            alert(`Delete unsuccessfully, ID number ${removeUserId} is not in the system.`)
+        }
     }
 }
 </script>
@@ -53,7 +59,7 @@ const deleteUser = async () => {
                 {{ user.username }}
             </button>
             <button class = "bg-red-400" @click="registerUser">+</button>
-            <button class = "bg-red-400" @click="deleteUser">delete user</button>
+            <button @click="deleteUser"><RiDeleteBin5Line/></button>
         </div>
     </div>
 </template>
