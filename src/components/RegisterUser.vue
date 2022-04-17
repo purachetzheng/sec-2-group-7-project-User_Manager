a75e4f9c672c913a2da12f3f18e4da493cf20f7d
Phumin63130500094 <chumphu.phumin@gmail.com>
2022-04-17 22:04:42 +0700

    update ui

--- src/components/RegisterUser.vue
+++ src/components/RegisterUser.vue
@@ -1,36 +1,53 @@
 <script setup>
-import {computed} from 'vue'
-defineEmits(['createUser', 'cancelRegister', 'updateUser'])
+import { computed } from 'vue';
+defineEmits(['createUser', 'cancelRegister', 'updateUser']);
 const props = defineProps({
-    currentUser:{
+    currentUser: {
         type: Object,
-        default: {}
-    }
-})
+        default: {},
+    },
+});
 
-const newUser = computed(() =>{
-    return {id: props.currentUser.id, username: props.currentUser.username, password: props.currentUser.password}
-})
+const newUser = computed(() => {
+    return { id: props.currentUser.id, username: props.currentUser.username, password: props.currentUser.password };
+});
 </script>
- 
+
 <template>
-    <h1 v-if = "newUser.id > 0">Editing User</h1>
-    <h1 v-else>Register User</h1>
-    <div>
-        <label>Username: </label>
-        <input id = "usernameText" type="text" placeholder="type your username here..." v-model = "newUser.username"/>
-    </div>
-    <div>
-        <label>Password: </label>
-        <input id = "passwordText" type="password" placeholder="type your password here..." v-model = "newUser.password"/>
-    </div>
-    <div>
-        <button v-if = "newUser.id > 0" @click = "$emit('updateUser', newUser)">Save</button> 
-        <button v-else @click = "$emit('createUser', newUser.username, newUser.password)">register</button>
-        <button @click = "$emit('cancelRegister', $event)">cancel</button>
+    <div class="absolute w-full h-full bg-gray-300 transition-all opacity-60"></div>
+    <div class="bg-red-300 absolute w-1/2 rounded-md">
+        <div class="flex flex-col p-4 w-full">
+            <div v-if="newUser.id > 0" class="">Editing User</div>
+            <div v-else class="font-bold">Add New User</div>
+            <div>
+                <label>Username: </label>
+                <input
+                    id="usernameText"
+                    class="input border-b-2 appearance-none border-gray-400 p-1 w-1/2"
+                    type="text"
+                    placeholder="type your username here..."
+                    v-model="newUser.username"
+                />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input
+                    id="passwordText"
+                    class="input border-b-2 appearance-none border-gray-400 p-1 pl-2 pr-2 w-1/2"
+                    type="password"
+                    placeholder="type your password here..."
+                    v-model="newUser.password"
+                />
+            </div>
+            <div class="flex gap-2">
+                <button v-if="newUser.id > 0" class="p-2 bg-gray-300 rounded-sm" @click="$emit('updateUser', newUser)">SAVE</button>
+                <button v-else class="p-2 bg-gray-300 rounded-sm" @click="$emit('createUser', newUser.username, newUser.password)">
+                    ADD
+                </button>
+                <button class="" @click="$emit('cancelRegister', $event)">CANCEL</button>
+            </div>
+        </div>
     </div>
 </template>
- 
-<style>
 
-</style>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,14 @@
 <script setup>
 import { ref, computed, reactive, nextTick, onBeforeMount, onMounted } from 'vue';
-import RiDeleteBin5Line from '../components/icons/RiDeleteBin5Line.vue'
-import IcSharpAddCircle from '../components/icons/IcSharpAddCircle.vue'
-import MdiTextBoxEdit from '../components/icons/MdiTextBoxEdit.vue'
-import RegisterUser from '../components/RegisterUser.vue'
+import RiDeleteBin5Line from '../components/icons/RiDeleteBin5Line.vue';
+import IcSharpAddCircle from '../components/icons/IcSharpAddCircle.vue';
+import MdiTextBoxEdit from '../components/icons/MdiTextBoxEdit.vue';
+import RegisterUser from '../components/RegisterUser.vue';
 //router
 import { useRoute, useRouter } from 'vue-router';
 const { params } = useRoute();
 const router = useRouter();
-const isShow = ref(false)
+const isShow = ref(false);
 //Get
 const users = ref([]);
 const getUsers = async () => {
@@ -29,128 +29,138 @@ const clickLink = (id) => {
 //CREATE
 const createNewUser = async (newUsername, newPassword) => {
     if (usernameText.value === '' && passwordText.value === '') {
-        alert('type your username and password')
-    }else if (usernameText.value === ''){
-        alert('type your username')
-    }
-    else if (passwordText.value === ''){
-        alert('type your password')
-    }else{
+        alert('type your username and password');
+    } else if (usernameText.value === '') {
+        alert('type your username');
+    } else if (passwordText.value === '') {
+        alert('type your password');
+    } else {
         const res = await fetch('http://localhost:5000/users', {
             method: 'POST',
             headers: {
-                'content-type': 'application/json'
+                'content-type': 'application/json',
             },
-            body: JSON.stringify({username: newUsername, password: newPassword})
-        })
-        if(res.status === 201){
+            body: JSON.stringify({ username: newUsername, password: newPassword }),
+        });
+        if (res.status === 201) {
             const addedUser = await res.json();
-            users.value.push(addedUser)
-            console.log("create account successfully");
-            alert(`User ${newUsername} has been created.`)
+            users.value.push(addedUser);
+            console.log('create account successfully');
+            alert(`User ${newUsername} has been created.`);
             location.reload();
-        }
-        else console.log('error, cannot create account');
-    } 
-}
+        } else console.log('error, cannot create account');
+    }
+};
 
 //DELETE
 const deleteUser = async () => {
-    console.log("delete working!");
-    let removeUserId = prompt("Enter user ID that you want to delete.")
-    if (removeUserId === null){
-        console.log("cancel");
+    console.log('delete working!');
+    let removeUserId = prompt('Enter user ID that you want to delete.');
+    if (removeUserId === null) {
+        console.log('cancel');
         return;
-    }else{
+    } else {
         const res = await fetch(`http://localhost:5000/users/${removeUserId}`, {
-            method: 'DELETE'
-        })
-        if(res.status===200){
-            users.value = users.value.filter((user) => user.id !== removeUserId)
+            method: 'DELETE',
+        });
+        if (res.status === 200) {
+            users.value = users.value.filter((user) => user.id !== removeUserId);
             console.log('delete succesfully');
-            alert(`Delete succesfully, user ID number ${removeUserId} has been removed`)
+            alert(`Delete succesfully, user ID number ${removeUserId} has been removed`);
             location.reload();
-        }else{
+        } else {
             console.log('error, cannot delete');
-            alert(`Delete unsuccessfully, user ID number ${removeUserId} is not in the system.`)
+            alert(`Delete unsuccessfully, user ID number ${removeUserId} is not in the system.`);
         }
     }
-}
+};
 
 //UPDATE
-const editingUser = ref({})
+const editingUser = ref({});
 const toEditingMode = async () => {
     console.log('editing work!');
-    let editUser = prompt("Enter user ID that you want to edit")
-    if (editUser === null){
+    let editUser = prompt('Enter user ID that you want to edit');
+    if (editUser === null) {
         return;
-    }else{
-        const res = await fetch (`http://localhost:5000/users/${editUser}`)
-        if(res.status === 200){
+    } else {
+        const res = await fetch(`http://localhost:5000/users/${editUser}`);
+        if (res.status === 200) {
             editingUser.value = await res.json();
-            isShow.value = true
-        }else{
-            alert(`Cannot edit since user ID number ${editUser} is not in the system.`)
-        } 
-        console.log(editingUser.value); 
+            isShow.value = true;
+        } else {
+            alert(`Cannot edit since user ID number ${editUser} is not in the system.`);
+        }
+        console.log(editingUser.value);
     }
-}
+};
 
 const modifyUser = async (editingUser) => {
     const res = await fetch(`http://localhost:5000/users/${editingUser.id}`, {
         method: 'PUT',
         headers: {
-            'content-Type': 'application/json'
+            'content-Type': 'application/json',
         },
         body: JSON.stringify({
             username: editingUser.username,
-            password: editingUser.password
-        })
-    })
-    if(res.status === 200){
-        const modifyUser = await res.json()
-        users.value = users.value.map((user) => user.id === modifyUser.id
-        ? {...user, username: modifyUser.username, password: modifyUser.password}
-        : user
-        )
-        alert(`User ID number ${modifyUser.id} has been edited.`)
-        isShow.value = false
+            password: editingUser.password,
+        }),
+    });
+    if (res.status === 200) {
+        const modifyUser = await res.json();
+        users.value = users.value.map((user) =>
+            user.id === modifyUser.id ? { ...user, username: modifyUser.username, password: modifyUser.password } : user
+        );
+        alert(`User ID number ${modifyUser.id} has been edited.`);
+        isShow.value = false;
     }
-} 
+};
 
 const callRegisterUser = () => {
     isShow.value = true;
-    console.log("register working!");
-}
+    console.log('register working!');
+};
 
 const cancelRegisterProcess = () => {
-    console.log("register cancel");
+    console.log('register cancel');
     isShow.value = false;
-    usernameText.value = ''
-    passwordText.value = ''
-    editingUser.value = ''
-}
+    usernameText.value = '';
+    passwordText.value = '';
+    editingUser.value = '';
+};
 </script>
 
 <template>
-    <div class="p-4">
-        <p class="text-xl">Profile</p>
-        <div class="flex space-x-4">
-            <button class="bg-red-400" v-for="(user, index) in users" :key="index" @click="clickLink(user.id)">
-                {{ user.username }}
-            </button>
-            <button @click="callRegisterUser" :disabled = "isShow"><IcSharpAddCircle/></button>
-            <button @click="toEditingMode" :disabled = "isShow"><MdiTextBoxEdit/></button>
-            <button @click="deleteUser"><RiDeleteBin5Line/></button>
+    <div class="flex flex-col container p-4">
+        <div class="flex flex-col space-y-4">
+            <div>
+                <p class="text-xl font-bold">Profile</p>
+            </div>
+            <div>
+                <div class="flex space-x-4">
+                    <button v-for="(user, index) in users" :key="index" @click="clickLink(user.id)">
+                        <div class="flex flex-col p-2 bg-gray-300 text-left hover:bg-gray-400">
+                            <span> ID: {{ user.id }} </span>
+                            <span> USERNAME: {{ user.username }} </span>
+                        </div>
+                    </button>
+                </div>
+            </div>
         </div>
-    </div>
-    <div v-show = "isShow">
-        <RegisterUser
-            @cancelRegister = "cancelRegisterProcess"
-            @createUser = "createNewUser"
-            :currentUser = "editingUser"
-            @updateUser = "modifyUser"
-        />
+        <div class="mt-4">
+            <div class="flex gap-2">
+                <button @click="callRegisterUser" :disabled="isShow"><IcSharpAddCircle /></button>
+                <button @click="toEditingMode" :disabled="isShow"><MdiTextBoxEdit /></button>
+                <button @click="deleteUser"><RiDeleteBin5Line /></button>
+            </div>
+            <div v-show="isShow" class="absolute min-h-screen top-0 left-0 w-full h-full flex justify-center items-center">
+                <RegisterUser
+                    @cancelRegister="cancelRegisterProcess"
+                    @createUser="createNewUser"
+                    :currentUser="editingUser"
+                    @updateUser="modifyUser"
+                />
+            </div>
+        </div>
     </div>
 </template>
 
